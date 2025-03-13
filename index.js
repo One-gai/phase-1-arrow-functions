1f56c618af9da16e4b278f83e0bb627539e8a2ae
Luke <luke.wangai@student.moringaschool.com>
2025-03-13 11:01:32 +0300

    Fat arrow functions complete

---
+++ index.js
@@ -0,0 +1,19 @@
+// const nums = [1,2,3,4,5,6,7,8,9,10];
+// const squares = nums.map(x => x **2);
+
+// console.log(nums);
+// console.log(squares);
+
+// finishedItems = overdueToDoItems.map(item => items.className = 'complete');
+// headers.innerText = `You finished ${finishedItems.length} items!`;
+
+
+// console.log(overdueToDoItems);
+
+
+const divide = function(){
+    return (2000/100)
+}
+
+const square = x => x*x
+const add = (l,w) => l + w
